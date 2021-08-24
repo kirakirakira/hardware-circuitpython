b27cc22a2524f05ae0ae1941422fabc3bb15dc43
Kira Hartlage <kira.hartlage@gmail.com>
2021-08-24 09:29:04 -0400

    try to make a class

---
+++ Bluefruit Examples/play-sine-wave.py
@@ -0,0 +1,39 @@
+import time
+import array
+import math
+import board
+import digitalio
+
+try:
+    from audiocore import RawSample
+except ImportError:
+    from audioio import RawSample
+
+try:
+    from audioio import AudioOut
+except ImportError:
+    try:
+        from audiopwmio import PWMAudioOut as AudioOut
+    except ImportError:
+        pass
+
+FREQUENCY = 440 # 440 Hz middle 'A'
+SAMPLERATE = 8000 # 8000 samples/second
+
+# Generate one period of sine wave
+length = SAMPLERATE // FREQUENCY
+sine_wave = array.array("H", [0] * length)
+for i in range(length):
+    sine_wave[i] = int(math.sin(math.pi * 2 * i / length) * (2 ** 15) + 2 ** 15)
+
+# Enable the speaker
+speaker_enable = digitalio.DigitalInOut(board.SPEAKER_ENABLE)
+speaker_enable.direction = digitalio.Direction.OUTPUT
+speaker_enable.value = True
+
+audio = AudioOut(board.SPEAKER)
+sine_wave_sample = RawSample(sine_wave)
+
+audio.play(sine_wave_sample, loop = True)
+time.sleep(3)
+audio.stop()
---
+++ Notes.md
@@ -0,0 +1,5 @@
+# Interesting Examples
+
+CircuitPython Easy: https://circuitpython.readthedocs.io/projects/circuitplayground/en/latest/api.html#adafruit_circuitplayground.circuit_playground_base.CircuitPlaygroundBase.play_tone
+CircuitPython a bit more control: https://learn.adafruit.com/adafruit-circuit-playground-bluefruit/circuitpython-audio-out
+C++ implementation of cp's `play_tone`: https://github.com/adafruit/Adafruit_CircuitPlayground/blob/649d03064a6e184f27b8d80a39d31101e3dbe51d/Adafruit_CircuitPlayground.cpp#L203
