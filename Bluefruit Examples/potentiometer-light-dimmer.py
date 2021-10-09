557ee588d2ebd4c94c065695da8dae8c4330bbb2
Kira Hartlage <kira.hartlage@gmail.com>
2021-10-09 13:16:55 -0400

    hook up the joystick for dimmer

--- Bluefruit Examples/potentiometer-light-dimmer.py
+++ Bluefruit Examples/potentiometer-light-dimmer.py
@@ -6,10 +6,12 @@ import analogio
 cp.pixels.brightness = 0.05
 cp.pixels.auto_write = False
 
+pink = (250, 50, 174)
+
 knob = analogio.AnalogIn(board.A6)
 
 while True:
-    cp.pixels.fill((255, 0, 0))
+    cp.pixels.fill(pink)
     pot_value = round(knob.value/65535 * 100)
     cp.pixels.brightness = pot_value/100
     cp.pixels.show()
--- PyLadies Presentation Examples/light-dimmer.py
+++ PyLadies Presentation Examples/light-dimmer.py
@@ -6,11 +6,25 @@ import analogio
 cp.pixels.brightness = 0.05
 cp.pixels.auto_write = False
 
-knob = analogio.AnalogIn(board.A6)
+pink = (250, 50, 174)
+blue = (0, 0, 255)
+green = (0, 255, 0)
+
+knob_x = analogio.AnalogIn(board.A6)
+knob_y = analogio.AnalogIn(board.A5)
 
 while True:
-    cp.pixels.fill((255, 0, 0))
-    pot_value = round(knob.value/65535 * 100)
-    cp.pixels.brightness = pot_value/100
+    pot_value_x = round(knob_x.value/65535 * 100)
+    pot_value_y = round(knob_y.value/65535 * 100)
+    print(pot_value_x, pot_value_y)
+    brightness = (pot_value_x + pot_value_y)/2000
+    print("brightness ", brightness)
+    cp.pixels.brightness = brightness
+    if brightness < 0.03:
+        cp.pixels.fill(blue)
+    elif brightness < 0.05:
+        cp.pixels.fill(green)
+    else:
+        cp.pixels.fill(pink)
     cp.pixels.show()
     time.sleep(0.1)
